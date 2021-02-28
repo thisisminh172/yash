9729fab75f1f32c49e77f230265fe646e6aa0fd0
thisisminh172 <thisisminh172@gmail.com>
2021-02-28 07:49:45 +0700

    ok

---
+++ yash.ViewModels/Catalog/Carts/CartViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace yash.ViewModels.Catalog.Carts
+{
+    public class CartViewModel
+    {
+        //this class is not have to use it
+        //public int Id { get; set; }
+        public int ItemId { get; set; }//FK
+        public string ItemName { get; set; }
+        public string GoldName { get; set; }
+        public string DiamondName { get; set; }
+        public string ProductTypeName { get; set; }
+
+        public int Quantity { get; set; }
+        public float Price { get; set; }
+        public int UserId { get; set; }//FK
+        public DateTime DateCreated { get; set; }
+    }
+}
