242fd79f5fe1e2bec033324bc8b5ae88b5a8597e
thisisminh172 <thisisminh172@gmail.com>
2021-03-07 19:11:41 +0700

    finished

--- yash.WebApp/Controllers/HomeController.cs
+++ yash.WebApp/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace yash.WebApp.Controllers
             return View();
         }
 
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
