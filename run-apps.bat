6e80eaf454b16f5cf6a1f64df86bab04252c182e
thisisminh172 <thisisminh172@gmail.com>
2021-03-18 08:31:27 +0700

    add sql

--- run-apps.bat
+++ run-apps.bat
@@ -8,11 +8,11 @@ ECHO ============================
 
 REM start cmd.exe /k "dotnet dev-certs https --trust"
 
-cd /d .\yash.WebApi
-start cmd.exe /k "dotnet run .\yash.WebApi.csproj --urls=http://localhost:58960 /property:Configuration=Release"
-cd /d .\yash.WebApp
-start cmd.exe /k "dotnet run .\yash.WebApp.csproj --urls=http://localhost:58667 /property:Configuration=Release"
-cd /d .\yash.AdminApp
-start cmd.exe /k "dotnet run .\yash.AdminApp.csproj --urls=http://localhost:62948 /property:Configuration=Release"
+cd /d ..\yash\yash.WebApi
+start cmd.exe /k "dotnet run --project .\yash.WebApi.csproj --urls=http://localhost:58960 /property:Configuration=Release"
+cd /d ..\yash\yash.WebApp
+start cmd.exe /k "dotnet run --project .\yash.WebApp.csproj --urls=http://localhost:58667 /property:Configuration=Release"
+cd /d ..\yash\yash.AdminApp
+start cmd.exe /k "dotnet run --project .\yash.AdminApp.csproj --urls=http://localhost:62948 /property:Configuration=Release"
 
 
